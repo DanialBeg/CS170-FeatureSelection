dd33af42adc260e3106606c33b81c5608a22bbad
DanialBeg <danny00b@gmail.com>
2021-03-09 00:11:27 -0800

    Implementing k-fold, converting CSV to df

--- featureselection.py
+++ featureselection.py
@@ -1,8 +1,11 @@
 import csv
+import pandas as pd
+
 
 def main():
     print('Welcome to Danial Beg\'s Feature Selection Algorithm!')
     f = input('\nType in the name of the file to test: ')
+    in_f = f
     f = open(f, 'r')
 
     # Reference for reading the file: https://docs.python.org/3/library/csv.html
@@ -15,7 +18,8 @@ def main():
     print('\nThis dataset has ' + str(r1) + ' features.\n\n'
                                             'Please wait while I normalize the data!')
 
-    search(r1)
+    # search(r1)
+    return kfold(in_f, r1)
 
 
 def search(rl):
@@ -42,6 +46,25 @@ def search(rl):
 
 
 def leave_one_out_cross_validation():
+    return 0
+
+
+def kfold(inf, rl):
+    # Reading in CSV into dataframe:
+    # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html
+    df = pd.read_csv(inf, header=None)
+    nr = len(df.index)
+
+    for i in range(0, nr):
+        df[0][i] = df[0][i].split()
+    print(df[0][0][1])
+    for j in range(0, rl):
+        print(df[0][0][j])
+
+    for i in range(0, nr):
+        obj_classify = df[0][i][1:rl]
+        label_obj_classify = df[0][i][0]
+
     return 0
 
 
