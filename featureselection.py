de7fd9ff1e6b10d500089fb92146720e37d5bb0b
Danial <danny00b@gmail.com>
2021-03-11 13:14:08 -0800

    Finished forward selecction, need to implement backward selection

--- featureselection.py
+++ featureselection.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 def main():
     print('Welcome to Danial Beg\'s Feature Selection Algorithm!')
-    f = 'CS170_small_special_testdata__95.txt'
+    f = 'CS170_small_special_testdata__96.txt'
     print('\nType in the name of the file to test: ')
     in_f = f
     f = open(f, 'r')
@@ -31,27 +31,31 @@ def search(inf, rl):
 
     for i in range(1, rl):
         max_accur = 0
-        s_c = set()
+        finalk = 0
         for k in range(1, rl):
             if k not in seen_features:
-                s_temp = seen_features.copy()
+                s_temp = copy.deepcopy(seen_features)
                 s_temp.add(k)
                 accur = leave_one_out_cross_validation(inf, rl, s_temp)
+                k_temp = k
                 print('Using feature(s) ' + str(s_temp) + ' accuracy is ' + str(accur))
+
                 # print(seen_features)
 
             if accur >= max_accur:
                 max_accur = accur
                 f_accur = accur
-                finalk = k
-
+                finalk = k_temp
+                # print('K is ' + str(k))
         seen_features.add(finalk)
         s_c = copy.deepcopy(seen_features)
         d[f_accur] = s_c
-        print('Feature set ' + str(s_c) + ' was best, accuracy is ' + str(f_accur))
+        print('Feature set ' + str(seen_features) + ' was best, accuracy is ' + str(f_accur))
+        # print(d)
+    print('\n')
     print('Finished search!! The best feature subset is ' + str(d[max(d.keys())]) +
           ' which has an accuracy of ' + str(max(d.keys())))
-    print(d)
+    # print(d)
 
 
 def leave_one_out_cross_validation(inf, c, seen):
@@ -59,7 +63,7 @@ def leave_one_out_cross_validation(inf, c, seen):
     # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html
     df = pd.read_csv(inf, header=None)
     nr = len(df.index)
-    print(c)
+    # print(c)
     corr_classified = 0
 
     for i in range(0, nr):
